ff3d902301d63757f6ad95ed58ca72a2174dc184
vamatya99 <vamatya99@gmail.com>
2022-06-27 11:16:10 -0700

    updated streaming function

--- tesla.py
+++ tesla.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import json
-import binascii
 from PIL import Image
-import io
-
+from os import path
+from multiprocessing import Process
+import time
 
 def custom_auth(url):
     options = webdriver.ChromeOptions()
@@ -16,9 +16,24 @@ def custom_auth(url):
         WebDriverWait(browser, 300).until(EC.url_contains('void/callback'))
         return browser.current_url
 
-def get_cached_token():
-    with open("cache.json") as cache:
-        return json.load(cache)["auth_token"]
+def write_json(data):
+    if data["shift_state"] != "D":
+        print("parked")
+        return
+    print(data)
+    if not path.isfile('streaming_data.json'):
+        file = open('streaming_data.json','w')
+        json.dump({"data":[]},file,indent=4)
+        file.close()
+    with open('streaming_data.json','r+') as f:
+        try:
+            file_data = json.load(f)
+        except:
+            print("file read exception")
+            exit(1)
+        file_data["data"].append(data)
+        f.seek(0)
+        json.dump(file_data,f,indent=4)
 
 def main():
     tesla = teslapy.Tesla("elon@tesla.com",cache_file="cache.json",authenticator=custom_auth)
@@ -37,13 +52,11 @@ def main():
     vehicle = tesla.vehicle_list()[0]
     # vehicle.sync_wake_up()
     print(vehicle["display_name"] + ' last seen ' + vehicle.last_seen()+ " at " + str(vehicle['charge_state']['battery_level']) + '% SoC')
-    # print(vehicle.get_charge_history())
-    # print(vehicle.get_latest_vehicle_data())
     # img_hex = vehicle.compose_image()
-    # img_bytes = binascii.unhexlify(img_hex)
-    # img_stream = io.BytesIO(img_bytes)
-    # img = Image.open(img_hex)
-    print(vehicle)
+    if vehicle.available():
+        vehicle.stream(write_json,indefinitely=True)
+    else:
+        print("vehicle offline")
 
 if __name__ == "__main__":
     main()
